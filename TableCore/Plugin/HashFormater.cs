4eb2c29192fa5a6b56b66b25cd83d15669fcd230
Vincent <borr3331@outlook.com>
2019-04-13 14:47:29 +0800

    update source file
    update latest source file

---
+++ TableCore/Plugin/HashFormater.cs
@@ -0,0 +1,33 @@
+﻿using LitJson;
+using System.Xml;
+
+namespace TableCore.Plugin
+{
+    public class HashFormater : IGenFormater
+    {
+        public bool ignoreCase = false;
+
+        public IExportData ExportData(string data, string comment)
+        {
+            return null;
+        }
+
+        public JsonData Format(string data, GTOutputCfg catgory)
+        {
+            if (ignoreCase)
+                return StringUtil.IgnoreCaseToHash(data);
+            else
+                return StringUtil.ToHash(data);
+        }
+
+        public void Init(XmlElement element)
+        {
+            ignoreCase = element.GetAttribute("ignore_case") == "true";
+        }
+
+        public bool IsValid(string data)
+        {
+            return true;
+        }
+    }
+}
---
+++ TableCore/Plugin/MaskDataFormater.cs
@@ -0,0 +1,48 @@
+﻿using LitJson;
+using System.Xml;
+
+namespace TableCore.Plugin
+{
+    public class MaskDataFormater : IGenFormater
+    {
+        string all = "abcdefghigklmnopqrstuvwxyz";
+
+        public void Init(XmlElement element)
+        {
+
+        }
+
+        public bool IsValid(string input)
+        {
+            return true;
+        }
+
+        public string FormatInput(string input)
+        {
+            if (input == "[ALL]")
+                return all;
+            return input == null ? "" : input.ToLower();
+        }
+
+        public JsonData Format(string input, GTOutputCfg category)
+        {
+            int num;
+            if (int.TryParse(input, out num))
+                return num;
+            int n = 0;
+            input = FormatInput(input);
+            int len = input == null ? 0 : input.Length;
+            for(int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                n |= 1 << (c - 'a');
+            }
+            return n;
+        }
+
+        public IExportData ExportData(string input, string comment)
+        {
+            return null;
+        }
+    }
+}
